4bab34a2021325644ad0f0f63f993012af010bb8
Phillip Tran <pntran@ucdavis.edu>
2020-04-23 11:51:45 -0700

    changed some variables around and used await instead of loader class

--- app_controller/pixi_frontend/src/ts/init_layout.ts
+++ app_controller/pixi_frontend/src/ts/init_layout.ts
@@ -23,6 +23,7 @@ class chunk_loader{
     hasNext(){
         return(this.idx < this.chunks.length);
     }
+
     write_chunk(to_write) {
         return(characteristic.writeValue(to_write))
         .catch(error => this.write_chunk(to_write))
@@ -31,6 +32,7 @@ class chunk_loader{
 
 let loader = new chunk_loader();
 var characteristic = null;
+var descriptor = null;
 
 function setup(){
 
@@ -55,10 +57,13 @@ function setup(){
 
     form.appendChild(file_input);
     form.appendChild(load_file_button);
+
+
 }
 
+
 function connect_to_device() {
-    var UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
+    var UUID = "8601cf7e-8291-4cb7-baef-cf570c53485f";
 
     let navigator: any = window.navigator
     navigator.bluetooth.requestDevice({acceptAllDevices:true,optionalServices: [UUID]})
@@ -72,45 +77,44 @@ function connect_to_device() {
     })
     .then(service => {
         console.log("Getting characteristics");
-        return service.getCharacteristic('beb5483e-36e1-4688-b7f5-ea07361b26a8');
+        return service.getCharacteristic('f2aec022-00ac-44b4-8158-07c48d2024c1');
     })
     .then(_characteristic => {
         console.log("getting characteristic");
         characteristic = _characteristic;
-        characteristic.startNotifications();
-        characteristic.addEventListener('characteristicvaluechanged',
-        send_next_bytes);
+        // characteristic.startNotifications();
+        // characteristic.addEventListener('characteristicvaluechanged',
+        // send_next_bytes);
         console.log(characteristic);
         let error = document.getElementById("error_msg");
         error.innerHTML = "Connected successfully! You can now vroom vroom the robot";
     })
-    .catch( _ => {
+    .catch( err => {
         let error = document.getElementById("error_msg");
-        error.innerHTML = "Failed to connect successfully! Please try again";
+        error.innerHTML = "Failed to connect successfully! Please try again: " + err;
     })
 }
 
-function send_next_bytes(event) { 
+var d1;
+var d2;
+
+async function send_next_bytes(event) { 
 
     var enc = new TextEncoder();
 
     if(!loader.hasNext()){
         console.log("all done!")
-        var currentdate = new Date(); 
-        var datetime = "Last Sync: " + currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
-        console.log(datetime)
+        
         loader.write_chunk(enc.encode("done"))
+        d2 = new Date() 
+        console.log("Took: " + ((d2.getTime() - d1.getTime())/1000) )
         return
     }
     console.log("sending next: ")
     var to_write = loader.next_chunk();
     console.log(to_write)
-    loader.write_chunk(to_write);
+    
+    await loader.write_chunk(to_write);
 }
 
 function load_file() {
@@ -122,24 +126,31 @@ function load_file() {
     fr.onload = receivedText;
     fr.readAsArrayBuffer(file);
 
-    function receivedText() {
+    async function receivedText() {
         var result = fr.result;
 
         // for printing time sending file took
         console.log("sending first chunk");
-        var currentdate = new Date(); 
-        var datetime = "Last Sync: " + currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
-        console.log(datetime)
-        loader.add_chunks(listify(result));
-        
-        characteristic.writeValue(loader.next_chunk());
+        d1 = new Date(); 
+        let res = listify(result)
+
+        for(var i = 0; i < res.length; i++){
+            let d3 = new Date();
+            await characteristic.writeValue(res[i])
+            let d4 = new Date();
+            console.log("Took: " + ((d4.getTime() - d3.getTime())) )
+        }
+        let enc = new TextEncoder();
+        await characteristic.writeValue(enc.encode("done"))
+        d2 = new Date();
+        console.log("Took Overall: " + ((d2.getTime() - d1.getTime())/1000) )
+        // loader.add_chunks(listify(result));
+        // await characteristic.writeValue(loader.next_chunk());
+
     }
 }
+
+
 
 function listify(result) {
     //split into 512 byte sized chunks
